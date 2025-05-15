dd476e119d3fe10381292c94dceb17f5bfeb8995
Mahananda Herundi <mskodekall07@gmail.com>
2025-05-15 09:25:06 -0700

    update

--- public/js/map.js
+++ public/js/map.js
@@ -3,15 +3,15 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
             container: "map", // container ID
             style: "mapbox://styles/mapbox/streets-v12", //style url
-            center:listing.geometry.coordinates , // starting position [lng, lat]. Note that lat must be set between -90 and 90
-            zoom: 2, // starting zoom
+            center:JSON.parse(coordinates) , // starting position [lng, lat]. Note that lat must be set between -90 and 90
+            zoom: 9, // starting zoom
     });
 
  // console.log(listing.geometry.coordinates);
    const marker = new mapboxgl.Marker({ color: "red"})
-      .setLngLat(listing.geometry.coordinates)
+      .setLngLat(JSON.parse(coordinates))
       .setPopup(new mapboxgl.Popup({offset: 25})
-      .setHTML(`<h4>${listing.location}</h4><p>Exact location provided after booking</p>`))
+      .setHTML("<p>Exact location provided after booking</p>"))
        .addTo(map);
 
 
